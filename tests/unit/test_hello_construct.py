ab9894fcfb5ef943d0dc090424efb93f0c8fc98f
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-09 11:15:17 +0300

    Added s3 bucket construct

--- tests/unit/test_hello_construct.py
+++ tests/unit/test_hello_construct.py
@@ -2,9 +2,9 @@ import unittest
 
 from aws_cdk import core
 
-from ec2_instance.ec2_instance_construct import EC2InstanceConstruct
+from s3_buckets_construct import S3BucletsConstruct
 
-class TestEC2Instance(unittest.TestCase):
+class TestS3Buclets(unittest.TestCase):
 
     def setUp(self):
         self.app = core.App()
@@ -12,5 +12,5 @@ class TestEC2Instance(unittest.TestCase):
     
     def test_num_buckets(self):
         num_buckets = 1
-        hello = EC2InstanceConstruct(self.stack, "Test1", num_buckets)
-        assert len(hello.buckets) == num_buckets
+        s3buckets = S3BucletsConstruct(self.stack, "Test1", num_buckets)
+        assert len(s3buckets.buckets) == num_buckets
