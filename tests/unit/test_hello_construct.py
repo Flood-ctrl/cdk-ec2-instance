df6c1121887103240e0a486a2eef293e2e3fe143
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-09 11:05:36 +0300

    Changed ec2 construct

--- tests/unit/test_hello_construct.py
+++ tests/unit/test_hello_construct.py
@@ -2,15 +2,15 @@ import unittest
 
 from aws_cdk import core
 
-from hello.hello_construct import HelloConstruct
+from ec2_instance.ec2_instance_construct import EC2InstanceConstruct
 
-class TestHelloConstruct(unittest.TestCase):
+class TestEC2Instance(unittest.TestCase):
 
     def setUp(self):
         self.app = core.App()
         self.stack = core.Stack(self.app, "TestStack")
     
     def test_num_buckets(self):
-        num_buckets = 10
-        hello = HelloConstruct(self.stack, "Test1", num_buckets)
+        num_buckets = 1
+        hello = EC2InstanceConstruct(self.stack, "Test1", num_buckets)
         assert len(hello.buckets) == num_buckets
