d15c3a92721bee08b8c3a0216000f59ab8bc6b2a
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-15 13:45:12 +0300

    Added CfnInclude for adding association manager template

--- ec2_instance/ssm_association_construct.py
+++ ec2_instance/ssm_association_construct.py
@@ -5,6 +5,7 @@ from aws_cdk import (
     aws_ssm as ssm,
 )
 
+
 class SSMAssociationConstruct(core.Construct):
 
     def __init__(self, scope: core.Construct, id: str, 
@@ -13,32 +14,29 @@ class SSMAssociationConstruct(core.Construct):
     **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        ssm_association = ssm.CfnAssociation(
-            self, "SSMAssociation",
-            name=ssm_association_name,
-            output_location=None,
-            ############################################
-            #Parameters are not adding to document!!!!!#
-            ############################################
-            parameters=core.IResolvable.resolve(
-                self,
-                {
-                    "SourceType": ["S3"],
-                    "SourceInfo":["{\"path\":\"s3://test-ansible-pl-hw/playbook.yml\"}"],
-                    "InstallDependencies":["True"],
-                    "PlaybookFile":["playbook.yml"],
-                    "ExtraVariables":["SSM=True"],
-                    "Check":["False"],
-                    "Verbose":["-v"]
-                },
-            ),
-            targets=[
-                ssm.CfnAssociation.TargetProperty(
-                    key="tag:CDK-Type",
-                    values=["EC2Instance"],
-                ),
-            ],
+        cfn_include = core.CfnInclude(
+            self, "CfnInclude",
+            template={
+                "Resources": {
+                    "SSMAssociation": {
+                        "Type" : "AWS::SSM::Association",
+                        "Properties" : {
+                            "AssociationName" : "SSMRunAnsible" ,
+                            "Name" : "AWS-RunAnsiblePlaybook",
+                            "Parameters" : {
+                                "playbookurl":["s3://test-ansible-pl-hw/playbook.yml"],
+                                "playbook":["playbook.yml"]
+                            },
+                            "Targets" : [{
+                                "Key": "tag:CDK-Type",
+                                "Values": ["EC2Instance"]
+                            }]
+                          }
+                    }
+                }
+            }
         )
+
 
         ec2_instance_name.add_to_role_policy(
             statement=iam.PolicyStatement(
