ac936fa6638ebea1c2afbf6e5c97110a5341b1d6
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-23 19:24:15 +0300

    Moved validation to function

--- lambda_ssm/ansible_run_command.py
+++ lambda_ssm/ansible_run_command.py
@@ -10,58 +10,7 @@ logging.basicConfig(level=logger.setLevel(os.environ.get('LOGLEVEL')),
 def handler(event, context):
     logger.debug(event)
     logger.info('Start executing handler')
-    ssm_client = boto3.client('ssm')
-    logger.info('ssm_client.send_command')
-    ssm_command_response = ssm_client.send_command(
-                Targets=[
-                    {
-                        'Key': 'tag:'+ os.environ['EC2_TAG_KEY'],
-                        'Values': [os.environ['EC2_TAG_VALUE']]
-                        }
-                    ],
-                #OutputS3BucketName=[os.environ['S3Output']],
-                DocumentName='AWS-RunAnsiblePlaybook',
-                Parameters={
-                    'playbookurl': [os.environ['PLAYBOOK_URL']],
-                    }, 
-        )
-    logger.debug(ssm_command_response)
-    logger.info('context.aws_request_id')
-    command_id = context.aws_request_id
-    print(ssm_command_response)
-    print(event) #CHANGEME
-
-    ec2 = boto3.client('ec2')
-    if 'detail-type' in event.keys():
-        if event['detail-type'] == 'EC2 Instance State-change Notification' and event['detail']['state'] == 'running':
-            print('YES')
-            ec2_instance_id = event['detail']['instance-id']
-            ec2_event_response = ec2.describe_instances(
-                Filters=[
-                {
-                    'Name': 'tag:'+ os.environ['EC2_TAG_KEY'],
-                    'Values':
-                        [os.environ['EC2_TAG_VALUE']],
-                },
-            ],
-                InstanceIds=[
-                       ec2_instance_id,
-                       ],)
-            #if ec2_event_response['Reservations']
-            print('PRINT RESERVATION')
-            if not ec2_event_response['Reservations']:
-                print('RESERVATION IS EMPTY')
-            else:
-                print(ec2_event_response['Reservations'])
-                print('RESERVATIOn TYPE')
-                print(type(ec2_event_response['Reservations']))
-
-    else:
-        print('THE OTHER TRIGGER')
-
-
-
-
+    cw_event_tag_validator(event, context)
     logger.info('Returning status code')
     return {
         'statusCode': 200,
@@ -71,3 +20,49 @@ def handler(event, context):
         'body': 'Done {}\n'.format(event)
     }
 
+
+def ssm_run_command_run_ansible_playbook(context):
+    ssm_client = boto3.client('ssm')
+    logger.info('AWS-RunAnsiblePlaybook')
+    ssm_command_response = ssm_client.send_command(
+        Targets=[
+            {
+                'Key': 'tag:'+ os.environ['EC2_TAG_KEY'],
+                'Values': [os.environ['EC2_TAG_VALUE']]
+                }
+            ],
+        DocumentName='AWS-RunAnsiblePlaybook',
+        Parameters={
+            'playbookurl': [os.environ['PLAYBOOK_URL']],
+            }, 
+        )
+    logger.debug(ssm_command_response)
+    command_id = context.aws_request_id
+    logger.debug(command_id)
+
+
+def cw_event_tag_validator(event, context):
+    logger.info('cw_event_tag_validator')
+    if 'detail-type' in event.keys():
+        if event['detail-type'] == 'EC2 Instance State-change Notification' \
+                                 and event['detail']['state'] == 'running':
+            logger.info('EC2 Instance State-change Notification')
+            ec2_instance_id = event['detail']['instance-id']
+            logger.debug(ec2_instance_id)
+            ec2 = boto3.client('ec2')
+            ec2_event_response = ec2.describe_instances(
+                Filters=[
+                    {
+                        'Name': 'tag:'+ os.environ['EC2_TAG_KEY'],
+                        'Values': [os.environ['EC2_TAG_VALUE']],
+                    },
+                ],
+                InstanceIds=[
+                    ec2_instance_id,
+                ],
+            )
+            logger.debug(ec2_event_response)
+            if ec2_event_response['Reservations'] != []:
+                ssm_run_command_run_ansible_playbook(context)
+    else:
+        ssm_run_command_run_ansible_playbook(context)
