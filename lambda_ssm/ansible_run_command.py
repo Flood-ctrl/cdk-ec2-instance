a7c357d16d7a7f88196f4b7cf849ac8244083ea6
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-23 15:07:34 +0300

    Added boto3 describe instances

--- lambda_ssm/ansible_run_command.py
+++ lambda_ssm/ansible_run_command.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import logging
 
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logger.setLevel(os.environ.get('LOGLEVEL')),
                     format='[%(levelname)s] : %(name)s : %(asctime)s : %(message)s',)
@@ -11,7 +12,7 @@ def handler(event, context):
     logger.info('Start executing handler')
     ssm_client = boto3.client('ssm')
     logger.info('ssm_client.send_command')
-    response = ssm_client.send_command(
+    ssm_command_response = ssm_client.send_command(
                 Targets=[
                     {
                         'Key': 'tag:'+ os.environ['EC2_TAG_KEY'],
@@ -24,11 +25,43 @@ def handler(event, context):
                     'playbookurl': [os.environ['PLAYBOOK_URL']],
                     }, 
         )
-    logger.debug(response)
+    logger.debug(ssm_command_response)
     logger.info('context.aws_request_id')
     command_id = context.aws_request_id
-    print(response)
-    print(event)
+    print(ssm_command_response)
+    print(event) #CHANGEME
+
+    ec2 = boto3.client('ec2')
+    if 'detail-type' in event.keys():
+        if event['detail-type'] == 'EC2 Instance State-change Notification' and event['detail']['state'] == 'running':
+            print('YES')
+            ec2_instance_id = event['detail']['instance-id']
+            ec2_event_response = ec2.describe_instances(
+                Filters=[
+                {
+                    'Name': 'tag:'+ os.environ['EC2_TAG_KEY'],
+                    'Values':
+                        [os.environ['EC2_TAG_VALUE']],
+                },
+            ],
+                InstanceIds=[
+                       ec2_instance_id,
+                       ],)
+            #if ec2_event_response['Reservations']
+            print('PRINT RESERVATION')
+            if not ec2_event_response['Reservations']:
+                print('RESERVATION IS EMPTY')
+            else:
+                print(ec2_event_response['Reservations'])
+                print('RESERVATIOn TYPE')
+                print(type(ec2_event_response['Reservations']))
+
+    else:
+        print('THE OTHER TRIGGER')
+
+
+
+
     logger.info('Returning status code')
     return {
         'statusCode': 200,
@@ -37,10 +70,4 @@ def handler(event, context):
         },
         'body': 'Done {}\n'.format(event)
     }
-
-
-def event_checker(event1):
-    dict_event = event1
-    if dict_event['detail-type'] == 'EC2 Instance State-change Notification' and dict_event['detail']['state'] == 'running':
-        print(dict_event)
 
