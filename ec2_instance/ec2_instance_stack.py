1922829aeeeedb100a98615cdf3d556cf7476b3b
serhii_novokhatskyi <serhii_novokhatskyi@epam.com>
2019-10-31 18:10:21 +0200

    removed variables

--- ec2_instance/ec2_instance_stack.py
+++ ec2_instance/ec2_instance_stack.py
@@ -6,26 +6,14 @@ from aws_cdk import (
     aws_logs as logs,
 )
 
-from s3_buckets_construct import S3BucketsConstruct
-from ssm_association_construct import SSMAssociationConstruct
-from lambda_ssm.lambda_ssm_construct import LambdaSsmConstruct
-from shared_vpc.shared_vpc_construct import SharedVpcConstruct
-
 
 class EC2Instance(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str,
-                 env: dict,
-                 ec2_tag_key="cdk", 
-                 ec2_tag_value=["instance"], 
-                 playbook_url=None,
-                 playbook_file_name=None,
-                 vpc_id: str =None,
-                 vpc_name: str =None,
-                 private_subnet_names: list =None,
-                 instances_count=1,
-                 ssm_policy=None,
-                 log_level='INFO',
+                 env: dict=None,
+                 ami_id: str=None,
+                 ec2_tag_key: str=None, 
+                 ec2_tag_value: str=None,
                  **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
@@ -36,12 +24,9 @@ class EC2Instance(core.Stack):
             string_parameter_name="centos-ami-id",
         )
 
-        ami_map_value = {aws_region: ami_id_parameter_store.string_value}
+        ami_id = ami_id_parameter_store.string_value
 
-        # if use_ami_id_from_ssm:
-            
-        # else:
-        #     ami_map_value = {aws_region: "ami-01d9d5f6cecc31f85"}
+        ami_id_value = {aws_region: ami_id}
 
         
         shared_subnet_1_id = ssm.StringParameter.from_string_parameter_name(
@@ -54,43 +39,44 @@ class EC2Instance(core.Stack):
             string_parameter_name="shared_subnet_2"
         )
 
+        def ec2_cfn_instance(id, 
+                             subnet_id: str=None,
+                             key_name: str=None,
+                             user_data: str=None,
+                             instance_name: str='ec2-instance', 
+                             instance_type: str='t2.micro') -> ec2.CfnInstance:
 
-        def ec2_cfn_nginx_instance(id, subnet_id, 
-                                   nginx_type: str='ec2-instance', 
-                                   instance_type: str='t2.micro') -> ec2.CfnInstance:
-
-            ec2_cfn_nginx_instance = ec2.CfnInstance(
+            ec2_cfn_instance = ec2.CfnInstance(
                 self, id,
-                image_id=ami_map_value[aws_region],
+                key_name=key_name,
+                user_data=user_data,
+                image_id=ami_id_value[aws_region],
                 instance_type=instance_type,
                 subnet_id=subnet_id,
                 tags=[
                     core.CfnTag(
                         key = 'Name',
-                        value = f'{nginx_type}-nginx'
+                        value = f'{instance_name}'
                     ),
                 ],
             )
 
-        ec2_cfn_nginx_instance(
+        ec2_cfn_instance(
             id = "EC2NginxInternal",
             subnet_id = shared_subnet_1_id.string_value,
-            nginx_type = "internal",
+            instance_name = "internal",
         )
 
-        ec2_cfn_nginx_instance(
-            id = "EC2NginxInternal2",
-            subnet_id = shared_subnet_2_id.string_value,
-            nginx_type = "external",
+        ec2_cfn_instance(
+            id = "TestWithoutSN",
+            instance_name = "TEST",
+            subnet_id='subnet-014b6cf8b1ccbda7b',
         )
 
-        ec2_tags = core.Tag.add(
-            self,
-            key=ec2_tag_key,
-            value=ec2_tag_value,
-            include_resource_types=["AWS::EC2::Instance"],
-        )
-
-#aws ec2 describe-images --region us-east-1 --owners 099720109477 --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-????????' 'Name=state,Values=available' | head -n50
-#aws ec2 describe-images --region us-east-1 --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text
-#arn:aws:iam::846580235911:instance-profile/AmazonSSMRoleForInstancesQuickSetup
+        if ec2_tag_key and ec2_tag_value is not None:
+            ec2_instance_tags = core.Tag.add(
+                self,
+                key=ec2_tag_key,
+                value=ec2_tag_value,
+                include_resource_types=["AWS::EC2::Instance"],
+            )
